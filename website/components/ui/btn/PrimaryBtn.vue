ec921adf4fcee7cb1a1cf0cc97e3b598c7f8a2a8
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-23 10:55:54 +0600

    Header - Default Layout updated

---
+++ website/components/ui/btn/PrimaryBtn.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    class="bg-primary text-white py-2 px-3 rounded-lg uppercase font-bold flex items-center gap-2"
+  >
+    <img :src="icon" :alt="title" />
+    {{ title }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+});
+</script>
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -2,16 +2,22 @@
 
 module.exports = {
   content: [
-    "./layouts/**/*.vue",
-    "./components/**/*.vue",
-    "./pages/**/*.vue",
-    "./renderer/**/*.vue",
+    './layouts/**/*.vue',
+    './components/**/*.vue',
+    './pages/**/*.vue',
+    './renderer/**/*.vue',
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#409EFF'
-      }
+        primary: '#0079F7',
+        'primary-light': '#3697FC',
+        'primary-dark': '#004388',
+        secondary: '#FFA600',
+        'secondary-light': '#FFC864',
+        'secondary-dark': '#C68000',
+        text: '#333333',
+      },
     },
   },
   plugins: [],
