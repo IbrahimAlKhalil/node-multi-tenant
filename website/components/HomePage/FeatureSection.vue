db0510a1c495a215597b96bbff0140b7741b85d1
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-23 17:54:59 +0600

    feat(website)[pages/home]: header and hero section added

---
+++ website/components/HomePage/FeatureSection.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="feature-section container">
+    <div class="w-9/12 p-5 rounded-lg shadow-xl flex"></div>
+  </div>
+</template>
--- website/components/ui/btn/SecondaryBtn.vue
+++ website/components/ui/btn/SecondaryBtn.vue
@@ -1,9 +1,9 @@
 <template>
   <button
-    class="bg-primary hover:bg-primary-dark transition duration-300 text-white py-2 px-3 rounded-lg uppercase font-bold flex items-center gap-2"
+    class="border border-secondary hover:bg-secondary transition-all duration-150 text-secondary hover:text-white py-2 px-3 rounded-lg uppercase font-bold flex items-center gap-2"
   >
     <component
-      class="inline-block mr-2 text-inherit transition-all duration-300"
+      class="inline-block mr-2 text-inherit transition-all duration-150"
       :is="icon"
     />
     <!-- <img :src="icon" :alt="title" /> -->
