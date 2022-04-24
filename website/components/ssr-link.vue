7c537506603248f18dc99539590dfc1d23d0cd7a
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-24 17:00:20 +0600

    chore(website)[components]: add ssr-link component

---
+++ website/components/ssr-link.vue
@@ -0,0 +1,24 @@
+<template>
+  <a :class="{ active: pageContext.urlPathname === href }" :href="href">
+    <slot />
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ssr-link',
+  props: {
+    href: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<script lang="ts" setup>
+import { usePageContext } from '#modules/use-page-context';
+const pageContext = usePageContext();
+</script>
