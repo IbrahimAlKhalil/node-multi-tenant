f3402e2b0b789763690efcfd456f4ac48dae666c
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-24 13:53:34 +0600

    refactor(website)[pages/home]: Footer added, hide scroll bottom from hero section and visible feature section

---
+++ website/components/Footer/PreFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class='py-3 bg-[#00000060] text-white'>
+    <p class='text-center'>{{ year }} &copy; All Right Reserve</p>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+})
+</script>
