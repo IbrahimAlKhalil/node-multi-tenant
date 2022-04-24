9da1c0f122ce86c1723d842552ab9fe6b2597dee
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-24 14:28:30 +0600

    build(website)[tailwind.config]: added primary-darker color

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         primary: '#0079F7',
         'primary-light': '#3697FC',
         'primary-dark': '#0056B0',
+        'primary-darker': '#012B53',
         secondary: '#FFA600',
         'secondary-light': '#FFC864',
         'secondary-dark': '#C68000',
