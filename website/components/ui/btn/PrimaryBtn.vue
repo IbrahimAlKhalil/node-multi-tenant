90501ff9af8a0fa7fc96da77bb459d5b0909f212
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-24 11:13:31 +0600

    refactor(website)[components/ui/btn]: change the btn props rule and some styles

--- website/components/ui/btn/PrimaryBtn.vue
+++ website/components/ui/btn/PrimaryBtn.vue
@@ -1,9 +1,10 @@
 <template>
   <button
-    class="bg-primary hover:bg-primary-dark transition duration-300 text-white py-2 px-3 rounded-lg uppercase font-bold flex items-center gap-2"
+    class="group bg-primary hover:bg-primary-dark transition duration-300 text-white py-2 px-3 rounded-lg uppercase font-bold flex items-center gap-2"
   >
     <component
-      class="inline-block mr-2 text-inherit transition-all duration-300"
+      class="inline-block mr-2 text-inherit transition-all duration-300 group-hover:scale-125"
+      :class="{ 'order-2': endIcon }"
       :is="icon"
     />
     <!-- <img :src="icon" :alt="title" /> -->
@@ -19,7 +20,12 @@ defineProps({
   },
   icon: {
     type: String,
-    required: true,
+    required: false,
+  },
+  endIcon: {
+    type: Boolean,
+    required: false,
+    default: false,
   },
 });
 </script>
--- website/components/ui/btn/SecondaryBtn.vue
+++ website/components/ui/btn/SecondaryBtn.vue
@@ -1,9 +1,9 @@
 <template>
   <button
-    class="border border-secondary hover:bg-secondary transition-all duration-150 text-secondary hover:text-white py-2 px-3 rounded-lg uppercase font-bold flex items-center gap-2"
+    class="group border border-secondary hover:bg-secondary transition-all duration-150 text-secondary hover:text-white py-2 px-3 rounded-lg uppercase font-bold flex items-center gap-2"
   >
     <component
-      class="inline-block mr-2 text-inherit transition-all duration-150"
+      class="inline-block mr-2 text-inherit transition-all duration-150 group-hover:scale-125"
       :is="icon"
     />
     <!-- <img :src="icon" :alt="title" /> -->
@@ -19,7 +19,7 @@ defineProps({
   },
   icon: {
     type: String,
-    required: true,
+    required: false,
   },
 });
 </script>
