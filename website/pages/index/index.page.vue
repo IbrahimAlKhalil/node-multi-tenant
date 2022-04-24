fff48a8c1e844a18a41b9be3dcbd4ce703fe6d43
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-24 16:37:08 +0600

    feat(website)[pages/home]: add best-services section

--- website/pages/index/index.page.vue
+++ website/pages/index/index.page.vue
@@ -2,11 +2,13 @@
   <layout-main>
     <hero-section />
     <services-section />
+    <best-services-section />
     <pricing-section />
   </layout-main>
 </template>
 
 <script lang="ts">
+import BestServicesSection from '#components/HomePage/BestServicesSection.vue';
 import ServicesSection from '#components/HomePage/ServicesSection.vue';
 import PricingSection from '#components/HomePage/PricingSection.vue';
 import HeroSection from '#components/HomePage/HeroSection.vue';
@@ -14,9 +16,10 @@ import LayoutMain from '#layouts/main.vue';
 
 import { defineComponent } from 'vue';
 
+
 export default defineComponent({
   name: 'index-page',
-  components: { PricingSection, LayoutMain, HeroSection, ServicesSection },
+  components: { BestServicesSection, PricingSection, LayoutMain, HeroSection, ServicesSection },
   setup() {
     return {};
   },
