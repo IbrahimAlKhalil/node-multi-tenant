b6e4b46c0bb2315c91a24f02024cf870ab56f26e
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-24 16:34:51 +0600

    chore(website)[database]: add cluster and institute schema

---
+++ website/prisma/migrations/20220314105036_init/migration.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "Cluster"
+(
+    id          uuid NOT NULL,
+    name        text NOT NULL,
+    description text,
+    host        text NOT NULL,
+    CONSTRAINT "Cluster_pkey" PRIMARY KEY (id)
+);
+
+CREATE TABLE "Institute"
+(
+    id          uuid NOT NULL,
+    "clusterId" uuid NOT NULL,
+    code        text NOT NULL,
+    name        text NOT NULL,
+    slug        text NOT NULL,
+    "database"  text NOT NULL,
+    CONSTRAINT "Institute_pkey" PRIMARY KEY (id),
+    CONSTRAINT "Institute_clusterId_fkey" FOREIGN KEY ("clusterId") REFERENCES "Cluster" (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE UNIQUE INDEX "Cluster_host_key" ON "Cluster" ("host");
+
+CREATE INDEX "Cluster_host_idx" ON "Cluster" ("host");
+
+CREATE UNIQUE INDEX "Institute_slug_key" ON "Institute" ("slug");
+
+CREATE UNIQUE INDEX "Institute_code_key" ON "Institute" ("code");
+
+CREATE INDEX "Institute_clusterId_idx" ON "Institute" ("clusterId");
