31291a0bc97bd47d491b90816e18d1ac6f79f952
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-24 16:59:19 +0600

    chore(website)[types]: add urlPathname to PageContext

--- website/types/page-context.ts
+++ website/types/page-context.ts
@@ -3,8 +3,9 @@ import { ConcreteComponent } from 'vue';
 // The `pageContext` that are available in both on the server-side and browser-side
 export type PageContext = {
   Page: ConcreteComponent;
-  pageProps?: Record<string, any>;
+  pageProps?: Record<string, unknown>;
   lang: 'en' | 'bn' | 'ar';
+  urlPathname: string;
   documentProps?: {
     title?: string;
     description?: string;
