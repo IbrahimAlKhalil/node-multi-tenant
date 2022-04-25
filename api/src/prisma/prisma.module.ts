7b530b3c89bf5c6f9cd29be019cea4f0ac683425
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-25 15:10:54 +0600

    chore(api)[prisma]: use institute service

--- api/src/prisma/prisma.module.ts
+++ api/src/prisma/prisma.module.ts
@@ -1,7 +1,9 @@
+import { InstituteModule } from '../institute/institute.module.js';
 import { PrismaService } from './prisma.service.js';
 import { Module } from '@nestjs/common';
 
 @Module({
+  imports: [InstituteModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -1,77 +1,20 @@
+import { InstituteService } from '../institute/institute.service.js';
 import { PrismaInstance } from '../types/prisma-instance';
 import { PrismaClient } from '../../prisma/client';
-import { Institute } from '../types/institute';
 import { Config } from '../config/config.js';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PrismaService {
   constructor(
+    private readonly instituteService: InstituteService,
     private readonly config: Config,
   ) {
-    this.init();
-  }
-
-  private institutes: { [key: string]: Institute } = {};
-  private instances: { [key: string]: PrismaInstance } = {};
-
-  public async getPrisma(instituteId: string): Promise<PrismaClient> {
-    let prisma: PrismaClient;
-
-    if (this.instances.hasOwnProperty(instituteId)) {
-      const instance = this.instances[instituteId];
-      instance.lastUsed = Date.now();
-      prisma = instance.prisma;
-    } else {
-      const { PrismaClient } = await import('../../prisma/client/index.js');
-      const config = this.config.postgres;
-      const institute = this.institutes[instituteId];
-
-      prisma = new PrismaClient({
-        datasources: {
-          db: {
-            url: `postgres://${config.user}:${config.password}@${config.host}:${config.port}/${institute.database}`,
-          },
-        },
-      });
-
-      this.instances[instituteId] = {
-        lastUsed: Date.now(),
-        prisma,
-      };
-    }
-
-    return prisma;
-  }
-
-  private async loadInstitutes() {
-    const { default: got } = await import('got');
-
-    const host = `https://${this.config.app.websiteHost}`;
-    const path = `/api/institute/by-cluster-id/${this.config.app.clusterId}`;
-    const endpoint = `${host}${path}`;
-
-    const institutes: Institute[] = await got
-      .get(endpoint, {
-        https: {
-          rejectUnauthorized: this.config.app.env === 'production',
-        },
-      })
-      .json();
-
-    for (const institute of institutes) {
-      this.institutes[institute.id] = institute;
-    }
-  }
-
-  private async init() {
-    await this.loadInstitutes();
-
     // Remove old instances that are not used for a while
     setInterval(() => {
       const now = Date.now();
 
-      for (const instituteId in this.institutes) {
+      for (const instituteId in this.instituteService.institutes) {
         if (this.instances.hasOwnProperty(instituteId)) {
           const instance = this.instances[instituteId];
 
@@ -81,5 +24,49 @@ export class PrismaService {
         }
       }
     }, 5 * 60 * 1000);
+  }
+
+  private instances: { [instituteId: string]: PrismaInstance } = {};
+
+  public async getPrisma(instituteId: string): Promise<PrismaClient | null> {
+    // Check if the institute exists
+    if (!this.instituteService.institutes.hasOwnProperty(instituteId)) {
+      // Institute does not exist
+      return null;
+    }
+
+    let prisma: PrismaClient;
+
+    if (this.instances.hasOwnProperty(instituteId)) {
+      // A prisma instance for this institute already exists
+      // Return it
+
+      const instance = this.instances[instituteId];
+      instance.lastUsed = Date.now();
+      prisma = instance.prisma;
+    } else {
+      // No prisma instance for this institute exists yet
+
+      const { PrismaClient } = await import('../../prisma/client/index.js');
+      const institute = this.instituteService.institutes[instituteId];
+      const config = this.config.postgres;
+
+      // Instantiate a new prisma instance
+      prisma = new PrismaClient({
+        datasources: {
+          db: {
+            url: `postgres://${config.user}:${config.password}@${config.host}:${config.port}/${institute.database}`,
+          },
+        },
+      });
+
+      // Add the new prisma instance to the instances list
+      this.instances[instituteId] = {
+        lastUsed: Date.now(),
+        prisma,
+      };
+    }
+
+    return prisma;
   }
 }
