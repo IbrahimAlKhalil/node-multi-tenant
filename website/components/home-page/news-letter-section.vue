d4c27a214b55694fd48705fa2cff6c6aeaf22ad4
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-25 14:27:43 +0600

    feat(website)[pages/home]: add news-letter-section in home page

---
+++ website/components/home-page/news-letter-section.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="my-10 bg-text text-white py-14">
+    <div class="container mx-auto text-center">
+      <h3 class="font-bold text-4xl mb-3">{{ t(title) }}</h3>
+      <p class="w-4/5 mx-auto text-gray-400 mb-5">{{ t(description) }}</p>
+      <form class="flex justify-center items-center w-1/2 mx-auto">
+        <input
+          type="text"
+          :placeholder="t(placeholder)"
+          class="block p-3 rounded-tl-lg rounded-bl-lg outline-0 focus:bg-amber-100 text-text"
+          style="
+            flex: 2 0 70%;
+            clip-path: polygon(0 0, 90% 0, 100% 100%, 0 100%);
+          "
+        />
+        <button
+          class="bg-secondary text-white font-bold py-3 px-10 rounded-tr-lg rounded-br-lg -ml-6"
+          style="
+            flex: 1 0 auto;
+            clip-path: polygon(0 0, 100% 0, 100% 100%, 21% 100%);
+          "
+        >
+          {{ t(button) }}
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+export default defineComponent({
+  name: 'news-letter-section',
+  setup() {
+    const i18n = useI18n();
+
+    return {
+      description: "homePage['news-letter-section']['description']",
+      subTitle: "homePage['news-letter-section']['sub-title']",
+      title: "homePage['news-letter-section']['title']",
+      button: "homePage['news-letter-section']['button']",
+      placeholder: "homePage['news-letter-section']['placeholder']",
+      t: i18n.t,
+    };
+  },
+});
+</script>
--- website/pages/index/index.page.vue
+++ website/pages/index/index.page.vue
@@ -6,12 +6,14 @@
     <key-features-section />
     <pricing-section />
     <our-clients-section />
+    <news-letter-section />
   </layout-main>
 </template>
 
 <script lang="ts">
 import BestServicesSection from '#components/home-page/best-services-section.vue';
 import OurClientsSection from '#components/home-page/our-clients-section.vue';
+import NewsLetterSection from '#components/home-page/news-letter-section.vue';
 import ServicesSection from '#components/home-page/services-section.vue';
 import KeyFeaturesSection from '#components/home-page/key-features.vue';
 import PricingSection from '#components/home-page/pricing-section.vue';
@@ -24,12 +26,13 @@ export default defineComponent({
   name: 'our-client-section',
   components: {
     BestServicesSection,
-    PricingSection,
-    LayoutMain,
-    HeroSection,
-    ServicesSection,
     KeyFeaturesSection,
+    NewsLetterSection,
     OurClientsSection,
+    ServicesSection,
+    PricingSection,
+    HeroSection,
+    LayoutMain,
   },
   setup() {
     return {};
