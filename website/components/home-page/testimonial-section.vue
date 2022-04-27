4d1636e66b29fd4c31e48f3f2b168fdab0bf8690
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-27 15:31:47 +0600

    refactor(website)[pages/home]: update testimonial section

--- website/components/home-page/testimonial-section.vue
+++ website/components/home-page/testimonial-section.vue
@@ -10,37 +10,59 @@
           {{ t(textContent.description) }}
         </p>
       </div>
-      <!--  class="testimonials mt-20 flex items-center text-center gap-5" -->
-      <div>
-        <!--         class="flex justify-center flex-nowrap overflow-hidden gap-10 py-20 testimonials" -->
-        <div class="testimonials py-20 text-center">
-          <!--   ============ Start Item     -->
+      <div class="content-area flex items-center gap-5">
+        <div class="prev-btn cursor-pointer" @click="slider.prev()">
+          <component
+            :is="LeftArrow"
+            class="text-4xl text-gray-400 transition duration-300 hover:text-text hover:scale-150"
+          ></component>
+        </div>
+        <div class="testimonial-slider testimonials">
           <div
-            v-for="item in items"
-            :key="item.id"
-            class="testimonials__item relative border border-primary p-5 rounded-lg shadow-lg md:mr-10"
+            class="testimonials__slider-1 border-2 border-gray-200 rounded-lg p-4 flex items-center gap-5"
+            v-for="(testimonial, index) in testimonials"
+            :key="index"
           >
-            <div
-              class="testimonials__item__icon w-[100px] border rounded-full p-3 border-inherit bg-white absolute -top-[50px] left-1/2 -translate-x-1/2"
-            >
-              <img :src="Icon" alt="testimonial-1" width="100%" />
+            <div class="media" style="flex: 1 0 40%">
+              <img
+                src="https://via.placeholder.com/450x250"
+                :alt="t(testimonial.company)"
+                width="100%"
+                class="object-cover rounded-lg"
+              />
             </div>
-            <div class="testimonials__item__content mt-[50px]">
-              <h4 class="font-bold text-2xl text-primary mb-3">
-                {{ t(item.title) }}
-              </h4>
-              <p>
-                {{ t(item.description) }}
-              </p>
-              <div class="author mt-5">
-                <p class="font-bold text-xl text-text">{{ t(item.author) }}</p>
-                <p class="text-text italic">{{ t(item.company) }}</p>
+            <div class="text" style="flex: 2 1 60%">
+              <h3 class="font-bold text-4xl text-primary mb-3">
+                {{ t(testimonial.company) }}
+              </h3>
+              <p class="text-text text-md">{{ t(testimonial.description) }}</p>
+              <div class="flex items-center justify-between mt-4">
+                <div class="author">
+                  <h4 class="font-bold text-xl">{{ t(testimonial.author) }}</h4>
+                  <p class="text-gray-500">
+                    {{ t(testimonial.author_position) }}
+                  </p>
+                </div>
+                <div class="rating flex items-center gap-1">
+                  <component
+                    v-for="n in 5"
+                    :key="n"
+                    :is="testimonial.rating <= n ? StarRegular : StarSolid"
+                    class="text-secondary"
+                  ></component>
+                </div>
               </div>
             </div>
           </div>
-          <!--   ============ End Item     -->
+        </div>
+        <div class="next-btn cursor-pointer" @click="slider.next()">
+          <component
+            :is="RightArrow"
+            class="text-4xl text-gray-400 transition duration-300 hover:text-text hover:scale-150"
+          ></component>
         </div>
       </div>
+      <div class="handler"></div>
     </div>
   </div>
 </template>
@@ -48,6 +70,8 @@
 <script lang="ts">
 import RightArrow from '#icons/solid/angle-right.svg';
 import LeftArrow from '#icons/solid/angle-left.svg';
+import StarRegular from '#icons/regular/star.svg';
+import StarSolid from '#icons/solid/star.svg';
 import Icon from '#images/quran.svg?url';
 import { defineComponent } from 'vue';
 import { useI18n } from 'vue-i18n';
@@ -55,67 +79,79 @@ import Siema from 'siema';
 
 export default defineComponent({
   name: 'testimonial-section',
+  data() {
+    return {
+      slider: {},
+    };
+  },
   //mounted
   mounted() {
     const slider = new Siema({
       selector: '.testimonials',
       duration: 200,
       easing: 'ease-out',
-      perPage: 2,
+      perPage: 1,
       startIndex: 0,
       draggable: true,
       multipleDrag: false,
       threshold: 20,
       loop: true,
       rtl: false,
-      // onInit: () => {},
-      // onChange: () => {},
     });
+    this.slider = slider;
   },
   setup() {
     const i18n = useI18n();
     const textContent = {
       title: "homePage['testimonial-section'].title",
-      subTitle: "homePage['testimonial-section'].subTitle",
+      subTitle: "homePage['testimonial-section']['sub-title']",
       description: "homePage['testimonial-section'].description",
     };
 
     const items = [
       {
         id: 1,
-        title: "homePage['testimonial-section']['items']['item-1']['title']",
+        company:
+          "homePage['testimonial-section']['items']['item-1']['company']",
         description:
           "homePage['testimonial-section']['items']['item-1']['description']",
         author: "homePage['testimonial-section']['items']['item-1']['author']",
-        company:
-          "homePage['testimonial-section']['items']['item-1']['company']",
+        author_position:
+          "homePage['testimonial-section']['items']['item-1']['author_position']",
+        rating: 4,
       },
       {
         id: 2,
-        title: "homePage['testimonial-section']['items']['item-2']['title']",
+        company:
+          "homePage['testimonial-section']['items']['item-2']['company']",
         description:
           "homePage['testimonial-section']['items']['item-2']['description']",
         author: "homePage['testimonial-section']['items']['item-2']['author']",
-        company:
-          "homePage['testimonial-section']['items']['item-2']['company']",
+        author_position:
+          "homePage['testimonial-section']['items']['item-2']['author_position']",
+        rating: 5,
       },
       {
         id: 3,
-        title: "homePage['testimonial-section']['items']['item-3']['title']",
+        company:
+          "homePage['testimonial-section']['items']['item-3']['company']",
         description:
           "homePage['testimonial-section']['items']['item-3']['description']",
         author: "homePage['testimonial-section']['items']['item-3']['author']",
-        company:
-          "homePage['testimonial-section']['items']['item-3']['company']",
+        author_position:
+          "homePage['testimonial-section']['items']['item-3']['author_position']",
+        rating: 3,
       },
     ];
 
     return {
-      t: i18n.t,
+      testimonials: items,
       textContent,
-      LeftArrow,
+      StarRegular,
       RightArrow,
-      items,
+      t: i18n.t,
+      LeftArrow,
+      StarSolid,
       Icon,
     };
   },
