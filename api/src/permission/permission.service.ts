7ffd94c1b56fe36e38a6094e224d4c8455166021
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-04-27 17:25:56 +0600

    chore(api)[permission]: load models

--- api/src/permission/permission.service.ts
+++ api/src/permission/permission.service.ts
@@ -1,4 +1,33 @@
+import { ModelNames } from './types/model-names';
 import { Injectable } from '@nestjs/common';
+import { Model } from './types/model';
+import { fileURLToPath } from 'url';
+import fs from 'fs/promises';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 @Injectable()
-export class PermissionService {}
+export class PermissionService {
+  constructor() {
+    this.loadModels();
+  }
+
+  public models: Partial<Record<ModelNames, Model<any, ModelNames>>> = {};
+
+  private async loadModels() {
+    const files = (
+      await fs.readdir(path.resolve(__dirname, './models'))
+    ).filter((file) => file.endsWith('.js'));
+    const regex = /-([a-z])/g;
+
+    for (const file of files) {
+      const model = await import(path.resolve(__dirname, './models', file));
+      const name = file
+        .replace('.js', '')
+        .replace(regex, (match, letter) => letter.toUpperCase());
+
+      this.models[name as ModelNames] = model.default;
+    }
+  }
+}
