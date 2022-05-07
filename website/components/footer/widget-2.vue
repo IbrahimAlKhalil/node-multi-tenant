e4c7b9c8093bbe155326e92483958f698349f159
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-07 12:43:03 +0600

    refactor(website)[layouts/footer]: update footer widgets header and list items

--- website/components/footer/widget-2.vue
+++ website/components/footer/widget-2.vue
@@ -1,19 +1,47 @@
 <template>
   <div class="widget widget_links">
-    <h4 class="widget-title">{{ $slot }}</h4>
+    <h4
+      class="widget-title text-2xl bold border-b-2 border-gray-500 pb-2 mb-5 relative after:h-2 after:w-1/2 after:bg-secondary after:absolute after:left-0 after:-bottom-1 after:rounded-r"
+    >
+      {{ title }}
+    </h4>
     <ul>
-      <li><a href="#">About Us</a></li>
-      <li><a href="#">Contact Us</a></li>
-      <li><a href="#">Privacy Policy</a></li>
-      <li><a href="#">Terms of Use</a></li>
+      <li
+        v-for="item in links"
+        :key="item.id"
+        class="py-1 list-disc list-inside"
+      >
+        <a :href="item.url">{{ item.title }}</a>
+      </li>
     </ul>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'widget-2',
-};
+<script setup>
+import { ref } from 'vue';
+const title = ref('Important Links');
+const links = ref([
+  {
+    id: 1,
+    title: 'About Us',
+    url: '#',
+  },
+  {
+    id: 2,
+    title: 'Contact Us',
+    url: '#',
+  },
+  {
+    id: 3,
+    title: 'Privacy Policy',
+    url: '#',
+  },
+  {
+    id: 4,
+    title: 'Terms of Use',
+    url: '#',
+  },
+]);
 </script>
 
 <style scoped></style>
