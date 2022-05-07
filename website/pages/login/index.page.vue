20287b9e7be4bb14e7a93dfdb479a862f44af0ac
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-07 20:03:13 +0600

    feat(website)[pages/login]: add login page

---
+++ website/pages/login/index.page.vue
@@ -0,0 +1,13 @@
+<template>
+  <layout-main>
+    <HeroSection />
+  </layout-main>
+</template>
+
+<script lang="ts" setup>
+import HeroSection from '#components/login-page/HeroSection.vue';
+
+import LayoutMain from '#layouts/main.vue';
+</script>
+
+<style scoped></style>
