4bd3f70a9d6e5140350af68d09cc5da9c542fbd4
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-07 20:02:04 +0600

    refactor(website)[pages/blog]: update blog page

--- website/components/blog-page/blog-hero.vue
+++ website/components/blog-page/blog-hero.vue
@@ -26,11 +26,54 @@
           </div>
         </div>
         <div
-          :class="{ 'visibility-hidden opacity-0 ': search }"
+          :class="{ 'hidden ': search }"
           class="featured-blogs transition duration-500"
         >
           <div class="intro flex items-center gap-5 mb-5">
             <h3 class="font-bold text-xl uppercase">Most Popular</h3>
+            <div
+              class="flex-grow h-[2px] bg-white rounded relative before:w-2 before:h-2 before:absolute before:-left-2 before:top-1/2 before:-translate-y-1/2 before:bg-white before:rounded-full"
+            ></div>
+          </div>
+          <div class="blogs flex justify-between flex-wrap gap-8 text-xs">
+            <article
+              v-for="blog in featuresBlogs"
+              :key="blog.id"
+              class="blog flex flex-col lg:flex-row text-center lg:text-left items-center gap-3 border border-secondary rounded-md p-2"
+              style="flex: 1 1 300px"
+            >
+              <div class="media" style="flex: 1 1 40%">
+                <img :src="blog.image" class="rounded-md" :alt="blog.title" />
+              </div>
+              <div class="content" style="flex: 1 1 60%">
+                <h3 class="font-bold text-lg text-secondary">
+                  {{ blog.title }}
+                </h3>
+                <p class="italic text-gray-500">Date: {{ blog.date }}</p>
+                <p class="description my-2 text-center lg:text-justify">
+                  {{ blog.description }}
+                </p>
+                <a
+                  href="#"
+                  class="font-bold text-secondary flex items-center justify-center lg:justify-start gap-2 uppercase transition hover:text-secondary-dark"
+                  >Read More
+                  <component
+                    :is="RightArrow"
+                    class="text-xl"
+                    style="bottom: 0"
+                  ></component>
+                </a>
+              </div>
+            </article>
+          </div>
+        </div>
+        <!--        Search Result -->
+        <div
+          :class="{ hidden: !search }"
+          class="featured-blogs transition duration-500 block"
+        >
+          <div class="intro flex items-center gap-5 mb-5">
+            <h3 class="font-bold text-xl uppercase">Search Result</h3>
             <div
               class="flex-grow h-[2px] bg-white rounded relative before:w-2 before:h-2 before:absolute before:-left-2 before:top-1/2 before:-translate-y-1/2 before:bg-white before:rounded-full"
             ></div>
--- website/layouts/blog-page.vue
+++ website/layouts/blog-page.vue
@@ -1,19 +1,37 @@
 <template>
-  <div class="container flex gap-5 mb-10">
+  <div
+    class="controller container flex items-center justify-center gap-2 text-2xl text-primary md:hidden cursor-pointer p-3"
+    @click="toggleFilter"
+  >
+    <component :is="FilterIcon"></component>
+    <span class="font-bold">Filter</span>
+  </div>
+  <div class="container flex gap-5 mb-10 relative">
     <main class="main-area" style="flex: 2 1 75%">
       <slot name="main" />
     </main>
     <aside class="sidebar hidden md:block" style="flex: 1 0 25%">
       <slot name="sidebar" />
     </aside>
+    <div
+      :class="{ hidden: !filterDrawer }"
+      class="h-full bg-[rgba(0,0,0,0.75)] w-full absolute top-0 right-0"
+      @click="toggleFilter"
+    >
+      <div class="w-1/2 h-full absolute top-0 right-0">
+        <slot name="sidebar" />
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import { ref } from 'vue';
+const filterDrawer = ref(false);
 
-export default defineComponent({
-  name: 'blog-layout',
-  components: {},
-});
+const toggleFilter = () => {
+  filterDrawer.value = !filterDrawer.value;
+};
+
+import FilterIcon from '#icons/solid/filter.svg';
 </script>
