02f2aeb13c1545678feb9edda7aede30eb2d61af
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-07 19:58:46 +0600

    feat(website)[components/ui]: add underline heading component

---
+++ website/components/ui/UnderlineHeading.vue
@@ -0,0 +1,19 @@
+<template>
+  <h4
+    class="widget-title text-2xl bold border-b-2 border-gray-500 pb-2 mb-5 relative after:h-2 after:w-1/2 after:bg-secondary after:absolute after:left-1/2 after:lg:left-0 after:-bottom-1 after:rounded after:lg:rounded-l-none after:-translate-x-1/2 after:lg:-translate-x-0"
+  >
+    {{ title }}
+  </h4>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  title: {
+    type: String,
+    default: '',
+    required: true,
+  },
+});
+</script>
