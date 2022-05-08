7212ff9faba3688c0f9034116efb89bac04fef1e
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-08 20:03:21 +0600

    feat(website)[components/input-field]: add input field in components

--- website/components/form-components/input-field.vue
+++ website/components/form-components/input-field.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full">
+  <div class="w-full relative">
     <label class="font-bold text-gray-700" for="email">
       {{ label }}
     </label>
@@ -7,25 +7,51 @@
       :type="type"
       :name="name"
       :value="value"
-      @input="$emit('on-input', $event.target.value)"
+      @input="$emit('on-input', $event)"
       class="block px-3 py-2 rounded-md text-white outline-0 border-b-2 border-white focus:border-primary w-full bg-transparent transition duration-300 ease-in placeholder-opacity-60"
       :class="{
         'border-red-500 focus:border-red-500': error,
       }"
-      :placeholder="label"
-      @focus="$emit('on-focus', 'username')"
-      @keypress="$emit('on-keypress', 'username')"
+      :placeholder="placeholder"
+      @focus="$emit('on-focus', name)"
+      @keypress="$emit('on-keypress', name)"
     />
     <p class="text-sm italic text-red-500 p-1">
       {{ error }}
     </p>
+    <div
+      class="absolute right-0 top-1/2 -translate-y-1/2"
+      :class="[isPasswordField ? 'visible opacity-100' : 'invisible opacity-0']"
+    >
+      <component
+        v-show="showPassword"
+        :is="EyeOpen"
+        class="text-2xl p-1 cursor-pointer"
+        @click="$emit('on-toggle-password', name)"
+      ></component>
+      <component
+        v-show="!showPassword"
+        :is="EyeClosed"
+        class="text-2xl p-1 cursor-pointer"
+        @click="$emit('on-toggle-password', name)"
+      ></component>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-defineEmits(['on-input', 'on-keypress', 'on-focus']);
+import EyeClosed from '#icons/solid/eye-slash.svg';
+import EyeOpen from '#icons/solid/eye.svg';
+import { ref } from 'vue';
+
+defineEmits(['on-input', 'on-keypress', 'on-focus', 'on-toggle-password']);
+
 defineProps({
   label: {
+    type: String,
+    default: '',
+  },
+  placeholder: {
     type: String,
     default: '',
   },
@@ -45,5 +71,13 @@ defineProps({
     required: true,
   },
   error: String,
+  isPasswordField: {
+    type: Boolean,
+    default: false,
+  },
+  showPassword: {
+    type: Boolean,
+    default: false,
+  },
 });
 </script>
---
+++ website/components/form-components/select-input.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="w-full relative">
+    <div
+      class="min-h-[50px] rounded-md text-white border-b-2 border-white"
+      @click="handleAreaClick"
+    >
+      <input
+        type="text"
+        readonly
+        :placeholder="selectedItems.length ? '' : label"
+        class="block px-3 py-2 outline-0 w-full bg-transparent placeholder-opacity-60 absolute top-0 bottom-0 left-0 right-0"
+        @click="isDropdownOpen = !isDropdownOpen"
+      />
+      <span
+        class="absolute top-1/2 right-0 -translate-y-1/2 text-white text-xl p-5 cursor-pointer"
+        @click="isDropdownOpen = !isDropdownOpen"
+      >
+        <component :is="Down"></component>
+      </span>
+      <div class="flex flex-wrap gap-2 w-4/5 relative left-0 p-3">
+        <span
+          v-for="item in selectedItems"
+          :key="item.value"
+          class="px-2 py-1 bg-primary rounded flex items-center gap-2"
+        >
+          <span>
+            {{ item.label }}
+          </span>
+          <component
+            :is="CrossIcon"
+            style="bottom: 0"
+            class="cursor-pointer"
+            :class="{ hidden: isSingleSelect }"
+            @click="$emit('on-remove', item.value)"
+          ></component>
+        </span>
+      </div>
+    </div>
+    <ul
+      class="my-5 p-3 absolute top-full left-0 w-full bg-white rounded-md z-10"
+      v-show="isDropdownOpen"
+    >
+      <li
+        v-for="option in options"
+        v-bind:key="option.value"
+        @click="$emit('on-select', option)"
+        class="flex items-center justify-between px-3 py-2 rounded-md cursor-pointer text-primary hover:bg-primary hover:text-white"
+        :class="{
+          'font-bold':
+            selectedItems.findIndex((item) => item.value === option.value) !==
+            -1,
+        }"
+      >
+        <span>
+          {{ option.label }}
+        </span>
+        <span>
+          <component
+            :is="TickIcon"
+            v-show="
+              selectedItems.findIndex((item) => item.value === option.value) !==
+              -1
+            "
+          ></component>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import CrossIcon from '#icons/solid/times-circle.svg';
+import TickIcon from '#icons/solid/check-circle.svg';
+import Down from '#icons/solid/caret-down.svg';
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const isDropdownOpen = ref(false);
+
+defineEmits(['on-select', 'on-remove']);
+
+const handleAreaClick = (e: Event) => {
+  e.stopPropagation();
+};
+
+const disabledDropdown = (event: KeyboardEvent) => {
+  if (event.key === 'Escape') {
+    isDropdownOpen.value = false;
+  }
+};
+
+onMounted(() => {
+  document.addEventListener('keydown', disabledDropdown);
+  document.addEventListener('click', () => {
+    isDropdownOpen.value = false;
+  });
+});
+onUnmounted(() => {
+  document.removeEventListener('keydown', disabledDropdown);
+  document.removeEventListener('click', () => {
+    isDropdownOpen.value = false;
+  });
+});
+
+defineProps({
+  options: {
+    type: Array,
+    default: () => [],
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+  selectedItems: {
+    type: Array,
+    default: () => [],
+  },
+  isSingleSelect: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style scoped></style>
