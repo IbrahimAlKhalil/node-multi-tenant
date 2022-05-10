25f66b11d62dd512988a58238a3bb49f041b0103
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-10 19:50:23 +0600

    feat(website)[components/form-components]: add textarea field for form input

---
+++ website/components/form-components/textarea-field.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="w-full relative">
+    <label class="font-bold text-gray-700" :for="name">
+      {{ label }}
+    </label>
+    <textarea
+      rows="5"
+      :name="name"
+      :value="value"
+      @input="$emit('on-input', $event)"
+      class="block w-full px-3 py-2 mt-3 rounded-md text-white outline-0 border-y-2 border-y-white focus:border-primary focus:border-x bg-transparent transition duration-300 ease-in placeholder-opacity-60"
+      :class="{
+        'border-red-500 focus:border-red-500': error,
+      }"
+      @focus="$emit('on-focus', name)"
+      @keypress="$emit('on-keypress', name)"
+    ></textarea>
+    <p class="text-sm italic text-red-500 p-1">{{ error }} &nbsp;</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import EyeClosed from '#icons/solid/eye-slash.svg';
+import EyeOpen from '#icons/solid/eye.svg';
+
+defineEmits(['on-input', 'on-keypress', 'on-focus', 'on-toggle-password']);
+
+defineProps({
+  label: {
+    type: String,
+    default: '',
+  },
+  name: {
+    type: String,
+    default: '',
+    required: true,
+  },
+  value: {
+    type: String,
+    default: '',
+    required: true,
+  },
+  error: String,
+});
+</script>
