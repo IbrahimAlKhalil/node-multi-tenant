810135b4df1f0cbc87c3304c65c11dc36f8a3572
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-12 19:38:05 +0600

    refactor(website)[component/ui/btn]: update button emit and props

--- website/components/ui/btn/primary-btn.vue
+++ website/components/ui/btn/primary-btn.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     class="group bg-primary hover:bg-primary-dark transition duration-300 text-white py-2 px-3 rounded-lg uppercase font-bold flex items-center gap-2"
-    @click="$emit('dialog-open')"
+    @click="$emit('handle-click')"
   >
     <component
       class="inline-block mr-2 text-inherit transition-all duration-300 group-hover:scale-125"
@@ -14,7 +14,7 @@
 </template>
 
 <script lang="ts" setup>
-defineEmits(['dialog-open']);
+defineEmits(['handle-click']);
 defineProps({
   title: {
     type: String,
