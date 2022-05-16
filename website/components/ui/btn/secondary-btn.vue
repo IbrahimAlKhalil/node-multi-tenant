30968514fd1e7afc11e37e974769eb9488e0142c
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-16 11:57:56 +0600

    refactor(website)[components/ui/btn]: update secondary btn with size

--- website/components/ui/btn/secondary-btn.vue
+++ website/components/ui/btn/secondary-btn.vue
@@ -1,7 +1,11 @@
 <template>
   <button
-    class="group border border-secondary hover:bg-secondary transition-all duration-150 text-secondary hover:text-white py-2 px-3 rounded-lg uppercase font-bold flex items-center gap-2"
+    class="group border border-secondary hover:bg-secondary transition-all duration-150 text-secondary hover:text-white uppercase font-bold flex items-center"
     @click="$emit('handle-click')"
+    :class="{
+      'py-2 px-3 rounded-lg gap-2': size === 'medium',
+      'py-1 px-3 rounded-md gap-1': size === 'small',
+    }"
   >
     <component
       class="inline-block mr-2 text-inherit transition-all duration-150 group-hover:scale-125"
@@ -14,6 +18,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { Size } from '#types/component-size';
 export default defineComponent({
   name: 'secondary-btn',
   props: {
@@ -24,6 +29,11 @@ export default defineComponent({
     icon: {
       type: String,
       required: false,
+    },
+    size: {
+      type: String as () => Size,
+      required: false,
+      default: 'medium',
     },
   },
   emits: ['handle-click'],
