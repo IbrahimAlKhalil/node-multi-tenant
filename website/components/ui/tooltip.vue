8739ffc9588a935cfd163e029e52a05f19a19df5
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-16 12:00:59 +0600

    refactor(website)[components/ui]: add tooltip component

---
+++ website/components/ui/tooltip.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="relative">
+    <slot />
+    <div
+      class="w-[18px] h-[18px] p-1 absolute -top-3 -right-3 bg-primary rounded-full flex items-center justify-center"
+    >
+      <p class="text-[10px] text-white text-center font-bold">
+        <span>{{ tooltipValue > 99 ? 99 : tooltipValue }}</span
+        ><sup v-show="tooltipValue > 99">+</sup>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'tooltip-ui',
+  props: {
+    tooltipValue: {
+      type: Number,
+      default: 0,
+    },
+  },
+});
+</script>
