d90f90418ec430bfccee899fde4c37b353f40148
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-19 19:13:14 +0600

    feat(website)[components/ui]: add tab components in ui folder

---
+++ website/components/ui/tab-component/tab.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <component :is="CommentIcon"></component>
+  </div>
+</template>
+
+<script lang="ts">
+import CommentIcon from '#icons/regular/comments.svg';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-tab',
+  setup() {
+    return {
+      CommentIcon,
+    };
+  },
+});
+</script>
