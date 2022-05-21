7b4de506db48432792ab1702cd5d5bd3a96275ff
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-21 19:41:33 +0600

    refactor(website)[pages/post]: update post page

--- website/pages/blog/post.page.vue
+++ website/pages/blog/post.page.vue
@@ -6,40 +6,50 @@
     <post-description-section :post-description="test" />
     <post-tags-section :post="post" />
     <post-share-section />
-    <the-tab />
-    <leave-comment-section />
-    <comments-layout :comments="commentsData" />
+    <the-tabs
+      :tab-data="tabData"
+      :active-tab="activeTab"
+      :handle-tab-change="handleTabChange"
+    />
+    <tab-body v-show="activeTab === 1">
+      <comment-section />
+    </tab-body>
   </layout-main>
 </template>
 
 <script lang="ts">
 import PostPageMetaDescriptionSection from '#components/post-page/meta-description.section.vue';
-import LeaveCommentSection from '#components/post-page/leave-comment-section.vue';
 import PostDescriptionSection from '#components/post-page/desction-section.vue';
+import CommentSection from '#components/post-page/comment-section/index.vue';
 import PostShareSection from '#components/post-page/share-section.vue';
 import PostIntroSection from '#components/post-page/intro-section.vue';
 import PostTagsSection from '#components/post-page/tags-section.vue';
 import PostHeroSection from '#components/post-page/hero-section.vue';
-import TheTab from '#components/ui/tab-component/tab.vue'
+import TabBody from '#components/ui/tab-component/tab-body.vue';
+import TheTabs from '#components/ui/tab-component/tabs.vue';
 import { usePageContext } from '#modules/use-page-context';
-import { defineComponent } from 'vue';
-import LayoutMain from '#layouts/main.vue';
-import CommentsLayout from '#layouts/comments-layout.vue';
+import ThumbsDown from '#icons/regular/thumbs-down.svg';
+import CommentIcon from '#icons/regular/comments.svg';
+import ThumbsUp from '#icons/regular/thumbs-up.svg';
+import AngryIcon from '#icons/regular/angry.svg';
+import SadIcon from '#icons/regular/sad-cry.svg';
 import { comment } from '#types/comment-type';
+import LayoutMain from '#layouts/main.vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'blog-post',
   components: {
-    CommentsLayout,
     PostPageMetaDescriptionSection,
     PostDescriptionSection,
-    LeaveCommentSection,
     PostShareSection,
     PostIntroSection,
     PostTagsSection,
     PostHeroSection,
+    CommentSection,
     LayoutMain,
-    TheTab,
+    TheTabs,
+    TabBody,
   },
 
   data() {
@@ -56,7 +66,20 @@ export default defineComponent({
         '<h3>This is test html data rendering</h3>' +
         '<p>This is test html data rendering</p>' +
         '</div>',
+      tabData: [
+        { id: 1, title: 'Comments', icon: CommentIcon, count: 10 },
+        { id: 2, title: 'Like', icon: ThumbsUp, count: 10 },
+        { id: 3, title: 'Dislike', icon: ThumbsDown, count: 10 },
+        { id: 4, title: 'Sad', icon: SadIcon, count: 50 },
+        { id: 5, title: 'Angry', icon: AngryIcon, count: 10 },
+      ],
+      activeTab: 1,
     };
+  },
+  methods: {
+    handleTabChange(id: number) {
+      this.activeTab = id;
+    },
   },
 
   setup() {
