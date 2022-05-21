ef73100a5f3922092a71cf26d897e8efac9b4a2f
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-21 19:42:33 +0600

    refactor(website)[components/ui/tab]: update tab components

--- website/components/ui/tab-component/tab.vue
+++ website/components/ui/tab-component/tab.vue
@@ -1,19 +1,25 @@
 <template>
-  <div>
-    <component :is="CommentIcon"></component>
-  </div>
+  <li
+    class="flex items-center gap-2 p-2 cursor-pointer hover:text-primary"
+    :class="{ 'border-b-2 border-b-primary text-primary': isActive }"
+    @click="$emit('handle-change', id)"
+    v-if="count > 0"
+  >
+    <component :is="icon"></component>
+    <span class="font-bold uppercase">{{ title }}</span>
+    <span class="text-sm bg-gray-300 rounded px-2">{{ count }}</span>
+  </li>
 </template>
 
 <script lang="ts">
-import CommentIcon from '#icons/regular/comments.svg';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'the-tab',
+  props: ['id', 'icon', 'title', 'count', 'isActive'],
+  emits: ['handle-change'],
   setup() {
-    return {
-      CommentIcon,
-    };
+    return {};
   },
 });
 </script>
