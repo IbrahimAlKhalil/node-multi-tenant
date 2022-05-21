3991289adfff4d5efc2dd3781d99453ffbb566df
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-21 19:37:17 +0600

    feat(website)[components/ui]: add tooltip component

--- website/components/ui/tooltip.vue
+++ website/components/ui/tooltip.vue
@@ -1,19 +1,8 @@
 <template>
-  <div class="relative">
+  <div class="tooltip">
     <slot />
-    <div
-      class="w-[18px] h-[18px] p-1 absolute bg-primary rounded-full flex items-center justify-center"
-      :class="{
-        '-top-3 -right-3': position === 'top-right',
-        '-top-3 -left-3': position === 'top-left',
-        '-bottom-3 -right-3': position === 'bottom-right',
-        '-bottom-3 -left-3': position === 'bottom-left',
-      }"
-    >
-      <p class="text-[10px] text-white text-center font-bold">
-        <span>{{ tooltipValue > 99 ? 99 : tooltipValue }}</span
-        ><sup v-show="tooltipValue > 99">+</sup>
-      </p>
+    <div class="tooltip__area bg-text text-light after:bg-text">
+      <slot name="tooltip" />
     </div>
   </div>
 </template>
@@ -22,16 +11,39 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'tooltip-ui',
-  props: {
-    tooltipValue: {
-      type: Number,
-      default: 0,
-    },
-    position: {
-      type: String,
-      default: 'top-right',
-    },
+  name: 'the-tooltip',
+  setup() {
+    return;
   },
 });
 </script>
+
+<style scoped>
+.tooltip {
+  position: relative;
+}
+.tooltip__area {
+  position: absolute;
+  width: 100%;
+  min-width: max-content;
+  top: -150%;
+  left: 0;
+  padding: 2px 5px;
+  border-radius: 0.25rem;
+  z-index: 2;
+  opacity: 0;
+}
+.tooltip__area::after {
+  content: '';
+  width: 15px;
+  height: 15px;
+  position: absolute;
+  bottom: -8px;
+  left: 8px;
+  transform: rotate(45deg);
+  z-index: -1;
+}
+.tooltip:hover .tooltip__area {
+  opacity: 1;
+}
+</style>
