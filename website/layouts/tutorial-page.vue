e01aa3a839304b4b06b8857acece9422455d21f9
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-26 20:10:27 +0600

    feat(website)[pages/tutorials]: add tutorilas page

---
+++ website/layouts/tutorial-page.vue
@@ -0,0 +1,28 @@
+<template>
+  <layout-main>
+    <div class="flex justify-between" style="padding-top: var(--header-height)">
+      <div class="left-sidebar w-1/5 min-h-full bg-gray-100">
+        <slot name="left-sidebar" />
+      </div>
+      <div class="main-body grow p-5">
+        <slot />
+      </div>
+    </div>
+  </layout-main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import LayoutMain from '#layouts/main.vue';
+
+export default defineComponent({
+  name: 'tutorial-page',
+  components: {
+    LayoutMain,
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
---
+++ website/pages/tutorials/index.page.vue
@@ -0,0 +1,34 @@
+<template>
+  <TutorialPage>
+    <template #left-sidebar>
+      <the-list>
+        <list-item title="This is test" spacing="px-5 py-3"></list-item>
+        <list-item title="This is test" spacing="px-5 py-3"></list-item>
+        <list-item title="This is test" spacing="px-5 py-3"></list-item>
+      </the-list>
+    </template>
+    <div>
+      <h3>Main body</h3>
+    </div>
+  </TutorialPage>
+</template>
+
+<script lang="ts">
+import TutorialPage from '#layouts/tutorial-page.vue';
+import { TheList } from '#components/ui/list';
+import { ListItem } from '#components/ui/list';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'tutorial-page',
+  components: {
+    TutorialPage,
+    TheList,
+    ListItem,
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
