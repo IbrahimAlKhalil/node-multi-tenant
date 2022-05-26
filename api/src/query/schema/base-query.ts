2c75b19480b67939c9f1a9515e1047a7805f1214
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-26 17:07:32 +0600

    refactor(api): rearrange the imports and add js extension

--- api/src/query/schema/base-query.ts
+++ api/src/query/schema/base-query.ts
@@ -1,17 +1,17 @@
 import { ModelNames } from '../types/model-names';
 import { findUnique } from './find-unique.js';
+import { createMany } from './create-many.js';
+import { updateMany } from './update-many.js';
+import { deleteMany } from './delete-many.js';
+import { aggregate } from './aggregate.js';
+import { deleteSchema } from './delete.js';
 import { findMany } from './find-many.js';
+import { groupBy } from './group-by.js';
+import { create } from './create.js';
+import { update } from './update.js';
+import { upsert } from './upsert.js';
+import { count } from './count.js';
 import Joi from 'joi';
-import { count } from './count';
-import { aggregate } from './aggregate';
-import { groupBy } from './group-by';
-import { create } from './create';
-import { createMany } from './create-many';
-import { update } from './update';
-import { updateMany } from './update-many';
-import { upsert } from './upsert';
-import { deleteSchema } from './delete';
-import { deleteMany } from './delete-many';
 
 export interface BaseQuery {
   type:
