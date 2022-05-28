0e415dbb1c9d8341706946bb5aefcfe81fed2340
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-28 20:06:53 +0600

    feat(website)[components/ui/pagination]: add pagination component

---
+++ website/components/ui/pagination/pagination-item.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    :class="{
+      'border-gray-200 bg-gray-50 text-text': !isActive,
+      'bg-primary border-primary text-white': isActive,
+    }"
+    class="p-1 border hover:border-primary hover:bg-primary hover:text-white font-bold transition-all rounded min-w-[30px] text-center cursor-pointer"
+  >
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'pagination-item',
+  props: {
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
---
+++ website/components/ui/pagination/pagination.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex justify-center gap-1">
+    <pagination-item
+      v-for="(item, index) in total"
+      :key="index"
+      :title="item"
+      :isActive="item === activePage"
+    ></pagination-item>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import PaginationItem from './pagination-item.vue';
+
+export default defineComponent({
+  name: 'the-pagination',
+  components: { PaginationItem },
+  props: {
+    total: {
+      type: Number,
+      default: 0,
+    },
+    activePage: {
+      type: Number,
+      default: 1,
+    },
+  },
+});
+</script>
