6f4f9120a81215b94a26ee7e1af089298dc442bc
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-05-31 16:09:41 +0600

    refactor(api): update auth.service test file

--- api/src/auth/auth.service.spec.ts
+++ api/src/auth/auth.service.spec.ts
@@ -15,4 +15,37 @@ describe('AuthService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('authenticate', () => {
+    it('should return null if csrf token is not provided', () => {
+      expect(service.authenticate('', '')).toBeNull();
+    });
+    it('should return null if csrf token is invalid', () => {
+      expect(service.authenticate('', 'invalid')).toBeNull();
+    });
+    // tokenKeyIndex
+    it('should return null if cookie does not contain the token key', () => {
+      expect(service.authenticate('', 'valid')).toBeNull();
+    });
+    // boundary
+    it('should return null if cookie does not contain the token', () => {
+      expect(service.authenticate('', 'valid')).toBeNull();
+    });
+    // jwtPayload
+    it('should return null if jwt is invalid', () => {
+      expect(service.authenticate('', 'valid')).toBeNull();
+    });
+    // prisma
+    it('should return null if institute is disabled or does not exist', () => {
+      expect(service.authenticate('', 'valid')).toBeNull();
+    });
+    // tokenExistsInDB
+    it('should return null if token does not exist in the database', () => {
+      expect(service.authenticate('', 'valid')).toBeNull();
+    });
+    // return
+    it('should return null if token does not exist in the database', () => {
+      expect(service.authenticate('', 'valid')).toBeNull();
+    });
+  });
 });
