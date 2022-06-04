4f2a5bffa3ff45b6c16a689702bedf2f8e068a98
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-04 16:32:46 +0600

    feat(website)[components/ui]: add collapse-expand list

---
+++ website/components/ui/collapse-expand-list/list.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="w-full">
+    <list-item
+      v-for="item of items"
+      :key="item.id"
+      :item="item"
+      :postCategory="postCategory"
+    />
+  </ul>
+</template>
+
+<script lang="ts">
+import ListItem from './list-item.vue';
+import { defineComponent } from 'vue';
+
+type listItems = {
+  id: string;
+  title: string;
+};
+
+export default defineComponent({
+  name: 'collapse-expand-list',
+  components: {
+    ListItem,
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => [] as listItems[],
+    },
+    postCategory: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+});
+</script>
