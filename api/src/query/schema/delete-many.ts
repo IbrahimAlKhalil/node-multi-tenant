4f163088d06d4bd94a5bfacfad956abc182c3bec
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-04 11:42:50 +0600

    fix(api)[query]: allow select and include in deleteMany

--- api/src/query/schema/delete-many.ts
+++ api/src/query/schema/delete-many.ts
@@ -1,5 +1,7 @@
 import Joi from 'joi';
 
 export const deleteMany = Joi.object({
-  where: Joi.object().optional(),
+  where: Joi.object().required(),
+  select: Joi.object().optional(),
+  include: Joi.object().optional(),
 }).required();
