76c489ab7b044fba87197e28fe114591f3707d7f
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-04 16:24:12 +0600

    feat(website)[components/blog-create]: add upload file component

---
+++ website/components/blog-create/uploadFile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="border-2 border-dashed rounded-lg border-gray-400 p-3 cursor-pointer"
+  >
+    <img :src="uploadIcon" alt="Upload Icon" class="opacity-40" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import uploadIcon from '#images/upload-icon.svg?url';
+export default defineComponent({
+  name: 'upload-file',
+
+  setup() {
+    return {
+      uploadIcon,
+    };
+  },
+});
+</script>
