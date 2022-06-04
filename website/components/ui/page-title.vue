087109b476ba5ca819a816c21d1b463b13e44a8e
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-04 19:21:44 +0600

    feat(website)[components/ui]: add page title component

---
+++ website/components/ui/page-title.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1
+    class="font-bold text-2xl lg:text-5xl text-center uppercase mb-3 text-primary dark:text-secondary"
+    style="font-family: 'times new roman', serif"
+  >
+    {{ title }}
+  </h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'page-title',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
