52b77368b8be0a503fa3bbcc8ba0b649aae13897
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-06 17:04:19 +0600

    style(api): run eslint

--- api/src/config/data/prisma.ts
+++ api/src/config/data/prisma.ts
@@ -2,9 +2,6 @@ import { getEnvNum } from '../get-env.js';
 
 export default function () {
   return {
-    maxAge: getEnvNum(
-      'PRISMA_MAX_AGE',
-      100 * 60 * 10,
-    ), // 10 minutes in milliseconds
+    maxAge: getEnvNum('PRISMA_MAX_AGE', 100 * 60 * 10), // 10 minutes in milliseconds
   };
 }
