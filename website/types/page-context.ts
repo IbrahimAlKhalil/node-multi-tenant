b9bb97f8fa366c8763725b83fb00069da9bbd322
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-09 19:34:34 +0600

    refactor(website)[types]: update page-context types

--- website/types/page-context.ts
+++ website/types/page-context.ts
@@ -1,9 +1,11 @@
+import { AbstractServiceOptions } from 'directus/dist/types/services';
 import { ConcreteComponent } from 'vue';
 
 // The `pageContext` that are available in both on the server-side and browser-side
 export type PageContext = {
   Page: ConcreteComponent;
   pageProps?: Record<string, unknown>;
+  schema: AbstractServiceOptions['schema'];
   lang: 'en' | 'bn' | 'ar';
   urlPathname: string;
   documentProps?: {
