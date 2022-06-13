e99a8c7c71c91d75daded4cbce9b2da9eeb41239
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-13 11:46:16 +0600

    chore(api)[prisma]: add method to get role permissions

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -1,8 +1,8 @@
+import { PrismaClient, RoleCrudPermission } from '../../prisma/client';
 import { InstituteService } from '../institute/institute.service.js';
 import { PrismaInstance } from '../types/prisma-instance';
 import { ModelNames } from '../query/types/model-names';
 import { DMMFClass } from '../../prisma/client/runtime';
-import { PrismaClient } from '../../prisma/client';
 import { Config } from '../config/config.js';
 import { Injectable } from '@nestjs/common';
 import { Model } from './types/model';
@@ -167,5 +167,38 @@ export class PrismaService {
         primaryKey,
       };
     });
+  }
+
+  public async getRolePermissions(instituteId: string): Promise<Required<PrismaInstance>['rolePermissions']> {
+    const prisma = await this.getPrisma(instituteId);
+
+    if (!prisma) {
+      return new Map();
+    }
+
+    if (this.instances[instituteId].rolePermissions) {
+      return this.instances[instituteId].rolePermissions as Required<PrismaInstance>['rolePermissions'];
+    }
+
+    const map: PrismaInstance['rolePermissions'] = new Map();
+    const roles = await prisma.role.findMany({
+      include: {
+        RolePermissions: true,
+      }
+    });
+
+    for (const role of roles) {
+      const permissions: Partial<Record<ModelNames, RoleCrudPermission>> = {};
+
+      for (const permission of role.RolePermissions) {
+        permissions[permission.table as ModelNames] = permission;
+      }
+
+      map.set(role.id, permissions);
+    }
+
+    this.instances[instituteId].rolePermissions = map;
+
+    return map;
   }
 }
