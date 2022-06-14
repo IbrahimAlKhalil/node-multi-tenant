0325a72f5c80ff1a1d6653176eba3235447ae3c7
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-14 20:16:25 +0600

    feat(website)[components/editor-js]: add image and quote components

---
+++ website/components/editor-js-components/image.vue
@@ -0,0 +1,19 @@
+<template>
+  <img :src="data.file.url" :alt="data.file.title" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-image',
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ website/components/editor-js-components/quote.vue
@@ -0,0 +1,25 @@
+<template>
+  <figure class="qmm_figure">
+    <blockquote class="qmm_blockquote">
+      <p>{{ data.text }}</p>
+    </blockquote>
+    <figcaption class="qmm_figcaption">
+      <em>{{ data.caption }}</em>
+    </figcaption>
+  </figure>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'the-quote',
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped></style>
