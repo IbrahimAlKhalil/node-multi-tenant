c28846e1674c77b8fe003cd3e758b7bf3fe0c6ce
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-15 20:05:52 +0600

    feat(website)[layouts/header]: add mobile menu in header component

--- website/layouts/partials/header.vue
+++ website/layouts/partials/header.vue
@@ -7,22 +7,29 @@
     <div
       class="flex justify-between items-center container mx-auto py-3 h-full"
     >
-      <div class="logo h-full">
+      <div class="logo h-full z-[1]">
         <a href="/" class="block h-full">
           <img class="h-full" :alt="t('common.app-title')" :src="Logo" />
         </a>
       </div>
 
-      <nav class="nav flex-grow hidden lg:block">
-        <ul class="flex items-center justify-center">
+      <nav
+        class="nav flex-grow lg:block"
+        :class="{
+          'fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full flex justify-center items-center bg-black/90 text-white':
+            activeMenu,
+          hidden: !activeMenu,
+        }"
+      >
+        <ul class="flex flex-col md:flex-row items-center justify-center">
           <li
-            class="m-0 p-0 group hover:text-primary"
+            class="m-0 py-2 md:p-0 group hover:text-primary text-2xl md:text-base"
             :class="{
               'dark:text-light dark:hover:text-secondary': isSticky,
               'text-primary': item.href === navData.currentPath && !isSticky,
               'dark:text-secondary text-primary':
                 item.href === navData.currentPath && isSticky,
-              'text-text': item.href !== navData.currentPath,
+              'text-white md:text-text': item.href !== navData.currentPath,
             }"
             v-for="item in navData.data"
             :key="item.href"
@@ -30,7 +37,6 @@
             <a
               :href="item.href"
               class="ml-2 p-2 rounded-sm font-bold text-md transition-all duration-150"
-              :class="{}"
             >
               <component
                 class="mr-1 text-inherit group-hover:scale-110 transition-scale duration-300 hidden xl:inline-block"
@@ -43,14 +49,20 @@
           </li>
         </ul>
       </nav>
-      <div class="action-area">
+      <div class="action-area z-[1]">
         <div
-          class="lg:hidden rounded-md p-2 border border-primary flex justify-center items-center group hover:bg-primary transition duration-150"
+          class="lg:hidden rounded-md p-2 border flex justify-center items-center group transition duration-150"
+          :class="{
+            'border-primary hover:bg-primary': !activeMenu,
+            'bg-secondary border-secondary hover:border-secondary': activeMenu,
+          }"
+          @click="activeMenu = !activeMenu"
         >
           <component
-            class="text-primary transition duration-150 group-hover:text-white text-4xl"
+            class="transition duration-150 group-hover:text-white text-2xl md:text-4xl"
+            :class="{ 'text-primary': !activeMenu, 'text-white': activeMenu }"
             style="bottom: 0"
-            :is="MenuBar"
+            :is="!activeMenu ? MenuBar : Cross"
           />
         </div>
         <a href="/login">
@@ -72,6 +84,7 @@ import DoorOpen from '#icons/duotone/door-open.svg';
 import { useNavData } from '#stores/navdata.store';
 import { onMounted, onUnmounted, ref } from 'vue';
 import MenuBar from '#icons/light/bars.svg';
+import Cross from '#icons/light/times.svg';
 import Logo from '#images/logo.svg?url';
 import { useI18n } from 'vue-i18n';
 
@@ -79,6 +92,7 @@ const { urlPathname } = usePageContext();
 const navData = useNavData();
 const i18n = useI18n();
 
+const activeMenu = ref(false);
 const headerHeight = ref(70);
 const isSticky = ref(false);
 const t = ref(i18n.t);
