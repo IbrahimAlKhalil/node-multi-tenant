3532f6197249b25dfa3a691433c749418f594a54
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-15 20:01:45 +0600

    feat(website)[components/ui]: add section title component

---
+++ website/components/ui/section-title.vue
@@ -0,0 +1,20 @@
+<template>
+  <h3
+    class="font-bold text-4xl md:text-5xl leading-normal text-primary dark:text-secondary my-5"
+  >
+    <slot />
+  </h3>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'section-title',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
