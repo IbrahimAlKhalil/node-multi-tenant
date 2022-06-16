5bcad632eafd743bd029dd907e9271349c14d172
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-16 13:01:01 +0600

    feat(website)[components/animated-reactions]: add common frame for reactions

---
+++ website/components/animated-reactions/emoji-frame.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="flex justify-center items-center overflow-hidden"
+    :class="sizeClass"
+  >
+    <span>
+      <slot />
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'emoji-frame',
+  props: {
+    size: {
+      type: Number,
+      default: 8,
+    },
+  },
+  computed: {
+    sizeClass() {
+      return `w-${this.size} h-${this.size}`;
+    },
+  },
+});
+</script>
