5c861456a649c0b40bd59de16acfab5462aed41a
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-26 18:15:51 +0600

    refactor(website)[pages/faq]: update faq page pagination, hero section and ohters

--- website/pages/faq/index.page.server.ts
+++ website/pages/faq/index.page.server.ts
@@ -1,14 +1,26 @@
 import { OnBeforeRender } from '#types/on-before-render';
 import { ItemsService } from 'directus';
+import url from 'url';
 
 export { onBeforeRender };
 
 const onBeforeRender: OnBeforeRender = async (pageContext) => {
+  const queryObj = url.parse(pageContext.url, true);
+  const page = Number(queryObj.query?.page);
   const questionService = new ItemsService('question', {
     schema: (pageContext as any)?.schema,
   });
 
+  const questionsAggregate = await questionService.readByQuery({
+    aggregate: {
+      count: ['*'],
+    },
+  });
+  console.log('questionsAggregate', questionsAggregate);
+
   const questions = await questionService.readByQuery({
+    limit: 10,
+    page,
     fields: [
       'id',
       'title',
@@ -21,11 +33,10 @@ const onBeforeRender: OnBeforeRender = async (pageContext) => {
     ],
   });
 
-  console.log(questions);
-
   return {
     pageContext: {
       pageProps: {
+        questionsAggregate,
         questions,
       },
     },
--- website/pages/faq/index.page.vue
+++ website/pages/faq/index.page.vue
@@ -1,9 +1,24 @@
 <template>
   <layout-main>
-    <hero-section />
+    <common-scrollable-hero>
+      <hero-section />
+    </common-scrollable-hero>
     <div class="container my-10">
-      <div class="flex flex-col items-center mb-8">
-        <primary-btn title="Frequently Asked Questions" />
+      <div
+        class="search-box my-5 md:w-1/2 mx-auto relative shadow-md border border-gray-50 rounded"
+      >
+        <input
+          type="text"
+          :value="search"
+          @input="$emit('update:search', $event.target.value)"
+          placeholder="Search Here..."
+          class="block w-full rounded-lg py-2 px-3 text-xl text-text outline-0 border-2 border-transparent focus:border-secondary"
+        />
+        <div
+          class="absolute top-1/2 right-5 -translate-y-1/2 text-text text-xl hidden md:block"
+        >
+          <component :is="SearchIcon"></component>
+        </div>
       </div>
       <the-accordion
         v-for="question of questions"
@@ -13,7 +28,7 @@
       />
       <the-pagination
         @change-page="handleChangePage"
-        :total="10"
+        :total="totalPage"
         :activePage="activePage"
       />
     </div>
@@ -22,52 +37,35 @@
 
 <script lang="ts">
 import { Pagination as ThePagination } from '#components/ui/pagination';
+import CommonScrollableHero from '#layouts/common-scrollable-hero.vue';
 import HeroSection from '#components/faq-page/hero-section.vue';
 import PrimaryBtn from '#components/ui/btn/primary-btn.vue';
 import TheAccordion from '#components/ui/accordion.vue';
+import SearchIcon from '#icons/light/search.svg';
 import LayoutMain from '#layouts/main.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'faq-page',
   components: {
-    TheAccordion,
+    CommonScrollableHero,
     ThePagination,
+    TheAccordion,
     HeroSection,
     LayoutMain,
     PrimaryBtn,
   },
-  props: ['questions'],
+  props: ['questions', 'questionsAggregate'],
   data() {
     return {
+      totalPage: Math.ceil(Number(this.questionsAggregate[0].count) / 10),
       activePage: 1,
-      topQuestions: [
-        {
-          id: 1,
-          title: 'This is testing question 01',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        },
-        {
-          id: 2,
-          title: 'This is testing question 02',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        },
-        {
-          id: 3,
-          title: 'This is testing question 03',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        },
-        {
-          id: 4,
-          title: 'This is testing question 04',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur s',
-        },
-      ],
     };
+  },
+  mounted() {
+    const currentPage = Number(location.search.split('=')[1]);
+    console.log('currentPage No: ', currentPage);
+    this.activePage = !isNaN(currentPage) ? currentPage : 1;
   },
   methods: {
     handleChangePage(page: number) {
@@ -76,7 +74,9 @@ export default defineComponent({
     },
   },
   setup() {
-    return {};
+    return {
+      SearchIcon,
+    };
   },
 });
 </script>
