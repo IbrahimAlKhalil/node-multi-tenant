d89d0f9410f412b5afa6874bb8ba8298f10a5e0d
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-06-26 18:18:04 +0600

    refactor(website)[pages]: update post pasge reaction's section

--- website/components/post-page/reactions-section.vue
+++ website/components/post-page/reactions-section.vue
@@ -5,25 +5,25 @@
   <div class="flex justify-center items-center">
     <emoji-frame :size="12">
       <EmojiLike
-        class="scale-[30%] md:scale-[40%] grayscale hover:grayscale-0 transition duration-300 cursor-pointer"
+        class="scale-[30%] grayscale hover:grayscale-0 transition duration-300 cursor-pointer"
         @click="$emit('handleClick', 'like')"
       />
     </emoji-frame>
     <emoji-frame :size="12">
       <EmojiWow
-        class="scale-[30%] md:scale-[40%] grayscale hover:grayscale-0 transition duration-300 cursor-pointer"
+        class="scale-[30%] grayscale hover:grayscale-0 transition duration-300 cursor-pointer"
         @click="$emit('handleClick', 'wow')"
       />
     </emoji-frame>
     <emoji-frame :size="12">
       <EmojiSad
-        class="scale-[30%] md:scale-[40%] grayscale hover:grayscale-0 transition duration-300 cursor-pointer"
+        class="scale-[30%] grayscale hover:grayscale-0 transition duration-300 cursor-pointer"
         @click="$emit('handleClick', 'sad')"
       />
     </emoji-frame>
     <emoji-frame :size="12">
       <EmojiAngry
-        class="scale-[30%] md:scale-[40%] grayscale hover:grayscale-0 transition duration-300 cursor-pointer"
+        class="scale-[30%] grayscale hover:grayscale-0 transition duration-300 cursor-pointer"
         @click="$emit('handleClick', 'angry')"
       />
     </emoji-frame>
