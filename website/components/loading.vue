7579a333bccf554267509095290c7ad3fa178449
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-07-17 16:46:40 +0600

    chore(website)[pages/tutorials]: improve data loading performance

---
+++ website/components/loading.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="font-bold p-4 bg-primary-dark text-white rounded text-center">
+    Loading...
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-loading',
+});
+</script>
