ee58d2df8fb9cdc92cd91db3c4658b0aa463637e
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-07-21 20:02:05 +0600

    refactor(website)[components/ui]: update section-title component design

--- website/components/ui/section-title.vue
+++ website/components/ui/section-title.vue
@@ -1,6 +1,6 @@
 <template>
   <h3
-    class="font-bold text-4xl md:text-5xl leading-normal text-primary dark:text-secondary my-5"
+    class="font-bold text-4xl md:text-5xl md:leading-normal text-primary dark:text-secondary my-5"
   >
     <slot />
   </h3>
