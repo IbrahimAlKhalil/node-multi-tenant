11a742a94cbfe7ab9b72d132805f197225b26c67
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-07-27 13:20:48 +0600

    chore(api)[minio]: add mime-type map

--- api/src/minio/mime.enum.ts
+++ api/src/minio/mime.enum.ts
@@ -1,5 +1,13 @@
-export enum MimeEnum {
+export enum MimeIdEnum {
   JPEG = 1,
   PNG,
   WEBP,
 }
+
+export const MimeTypeMap: {
+  [key: number]: string;
+} = {
+  1: 'image/jpeg',
+  2: 'image/png',
+  3: 'image/webp',
+};
