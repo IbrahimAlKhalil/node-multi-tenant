163a350724d9bca61dcaa0f704ed2f465493034d
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-07-28 19:19:02 +0600

    chore(api)[minio]: rename MimeType to ImageMimeType, add image/gif, image/avif, image/tiff to image MimeType, add OtherMimeTypes

--- api/src/minio/mime-type.enum.ts
+++ api/src/minio/mime-type.enum.ts
@@ -1,5 +1,12 @@
-export enum MimeType {
+export enum ImageMimeType {
   'image/jpeg' = 1,
   'image/png',
   'image/webp',
+  'image/gif',
+  'image/avif',
+  'image/tiff',
+}
+
+export enum OtherMimeTypes {
+  'application/octet-stream' = 1000,
 }
