7f15af72be406723a338da650c1a9ba428bc1e3f
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-07-31 17:48:07 +0600

    build(web): configure vite clientPort

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,15 +1,22 @@
-import { fileURLToPath, URL } from "url";
-
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import { fileURLToPath, URL } from 'url';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    hmr: {
+      port: 24679,
+      clientPort: 24679,
+      host: '127.0.0.1',
+      protocol: 'ws',
     },
   },
 });
