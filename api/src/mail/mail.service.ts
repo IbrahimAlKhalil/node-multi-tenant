b773951411ccbecd80022a227ee0082f3764ba21
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-08-01 11:21:02 +0600

    fix(api)[mail]: import using file instead of directory

--- api/src/mail/mail.service.ts
+++ api/src/mail/mail.service.ts
@@ -1,4 +1,6 @@
-import SMTPTransport from 'nodemailer/lib/smtp-transport';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import SMTPTransport from 'nodemailer/lib/smtp-transport/index.js';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
