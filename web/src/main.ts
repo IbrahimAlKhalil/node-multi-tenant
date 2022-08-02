9beb47ce75a0f16ba4e03e0967e6de76967c863f
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-08-02 12:12:14 +0600

    chore(web): install and configure vue-i18n

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,12 +1,19 @@
-import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 import { createPinia } from 'pinia';
+import { createApp } from 'vue';
 
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+});
+
 app.use(createPinia());
 app.use(router);
+app.use(i18n);
 
 app.mount('#app');
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,23 @@
+import { vueI18n } from '@intlify/vite-plugin-vue-i18n';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { fileURLToPath, URL } from 'url';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueI18n({
+      compositionOnly: true,
+      include: path.resolve(
+        path.dirname(fileURLToPath(import.meta.url)),
+        './src/i18n/**',
+      ),
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
