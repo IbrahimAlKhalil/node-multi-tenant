7424053d9162e345443c4bebfde7c957f49d5034
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-08-04 20:15:34 +0600

    feat(api)[prisma/migrations]: add verification migration

--- api/prisma/migrations/20220804044057_verification/migration.sql
+++ api/prisma/migrations/20220804044057_verification/migration.sql
@@ -1,30 +1,27 @@
 /*
   Warnings:
 
-  - You are about to drop the `Varification` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `PasswordReset` table. If the table is not empty, all the data it contains will be lost.
 
 */
 -- CreateEnum
 CREATE TYPE "verification_type" AS ENUM ('PASSWORD_RESET', 'MOBILE', 'EMAIL');
 
 -- DropForeignKey
-ALTER TABLE "Varification" DROP CONSTRAINT "Varification_userId_fkey";
+ALTER TABLE "PasswordReset" DROP CONSTRAINT "PasswordReset_userId_fkey";
 
 -- DropTable
-DROP TABLE "Varification";
-
--- DropEnum
-DROP TYPE "varification_type";
+DROP TABLE "PasswordReset";
 
 -- CreateTable
 CREATE TABLE "Verification" (
     "id" SERIAL NOT NULL,
     "userId" INTEGER NOT NULL,
     "type" "verification_type" NOT NULL,
-    "payload" VARCHAR(400) NOT NULL,
+    "target" VARCHAR(400) NOT NULL,
     "token" INTEGER NOT NULL,
     "key" VARCHAR(32) NOT NULL,
-    "sentAt" TIMESTAMPTZ(6) NOT NULL,
+    "sentAt" TIMESTAMPTZ(6),
     "sentCount" INTEGER NOT NULL DEFAULT 0,
     "expireAt" TIMESTAMPTZ(6) NOT NULL,
     "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
