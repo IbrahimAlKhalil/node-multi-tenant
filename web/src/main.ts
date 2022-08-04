56be5b07f33607dcf0ec0e0bdb4631581b79e9eb
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-08-04 15:47:26 +0600

    chore(web): import base css and dark css

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,8 +1,14 @@
+import { vLoading } from 'element-plus/es/components/loading/src/directive';
 import { createI18n } from 'vue-i18n';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import router from './router';
 import App from './App.vue';
+
+import 'element-plus/theme-chalk/dark/css-vars.css';
+import 'element-plus/theme-chalk/base.css';
+
+import 'element-plus/theme-chalk/el-loading.css';
 
 const app = createApp(App);
 
@@ -10,6 +16,7 @@ const i18n = createI18n({
   legacy: false,
 });
 
+app.directive('loading', vLoading);
 app.use(createPinia());
 app.use(router);
 app.use(i18n);
