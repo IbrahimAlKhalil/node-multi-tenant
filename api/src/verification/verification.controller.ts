92255ad4f0bed8d156044e149028bdecb5a7a75d
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-08-07 11:45:46 +0600

    refactor(api): format and cleanup

--- api/src/verification/verification.controller.ts
+++ api/src/verification/verification.controller.ts
@@ -7,12 +7,12 @@ import { MailService } from '../mail/mail.service.js';
 import { AuthService } from '../auth/auth.service.js';
 import { SmsService } from '../sms/sms.service.js';
 import { Request, Response } from 'hyper-express';
-import { Config } from '../config/config.js';
+import { verifySchema } from './schema/verify.js';
 import { Session } from '../types/session.js';
+import { Config } from '../config/config.js';
 import { Injectable } from '@nestjs/common';
 import { UserKind } from 'prisma/client';
 import { Uws } from '../uws/uws.js';
-import { verifySchema } from './schema/verify.js';
 
 @Injectable()
 export class VerificationController {
@@ -141,8 +141,6 @@ export class VerificationController {
       convert: true,
     });
 
-    console.log({ error, value });
-
     if (error || !value) {
       // Validation failed
       throw new InputInvalid(error.message, error.details);
@@ -176,93 +174,5 @@ export class VerificationController {
 
     if (value?.key && verification.key === value.key)
       res.status(200).json({ key: value.key });
-
-    //   const identity = this.helperService.getIdentityType(
-    //     value.type === 'PASSWORD_RESET' ? value.emailOrMobile : value.target,
-    //   );
-
-    //   if (value.type === 'PASSWORD_RESET' && identity.type === 'username') {
-    //     throw new InputInvalid(
-    //       'Username is not allowed, you have to provide either email or mobile number',
-    //     );
-    //   }
-
-    //   const user = await prisma.user.findUnique({
-    //     where: {
-    //       [value.type === 'PASSWORD_RESET' ? identity.type : 'id']:
-    //         value.type === 'PASSWORD_RESET'
-    //           ? identity.value
-    //           : (session as Session<UserKind>).uid,
-    //     },
-    //   });
-
-    //   if (!user || user.disabled) {
-    //     throw new InputInvalid('Username or institute id invalid!');
-    //   }
-
-    //   const token = await this.helperService.generateRandomNumber(6);
-
-    //   const key = await this.helperService.generateRandomString(
-    //     this.config.auth.csrfTokenLength,
-    //   );
-
-    //   const verification = await prisma.verification.create({
-    //     data: {
-    //       userId: user.id,
-    //       key,
-    //       token,
-    //       type: value.type,
-    //       expireAt: new Date(Date.now() + this.config.verification.expireAt),
-    //       target:
-    //         value.type === 'PASSWORD_RESET' ? value.emailOrMobile : value.target,
-    //     },
-    //   });
-
-    //   if (identity.type === 'mobile') {
-    //     await this.smsService.send(
-    //       `Your verification code is ${token}. Do not share with others.`,
-    //       identity.value,
-    //     );
-    //   } else if (identity.type === 'email') {
-    //     let subject = 'Email Verification - QmmSoft';
-    //     let template = 'templates/email-verification.mjml';
-
-    //     if (value.type === 'PASSWORD_RESET') {
-    //       subject = 'Password Reset - QmmSoft';
-    //       template = 'templates/reset-password.mjml';
-    //     }
-
-    //     const job = await this.mailService.queue.add(
-    //       {
-    //         from: this.config.mail.passwordResetFrom,
-    //         to: identity.value,
-    //         subject,
-    //         template,
-    //         variables: {
-    //           code: token,
-    //           url: `${this.config.app.websiteOrigin}/verification?&id=${verification.id}&key=${key}&token=${token}&userId=${user.id}`,
-    //         },
-    //       },
-    //       {
-    //         jobId: verification.id,
-    //       },
-    //     );
-
-    //     job.finished().then(async () => {
-    //       await prisma.verification.update({
-    //         where: {
-    //           id: verification.id,
-    //         },
-    //         data: {
-    //           sentAt: new Date(),
-    //           sentCount: verification.sentCount + 1,
-    //         },
-    //       });
-    //     });
-    //   }
-
-    //   res
-    //     .status(200)
-    //     .json({ key, id: verification.id, userId: user.id, success: true });
   }
 }
