a91b642c3f88e869bdef41fc2f0a0358449b8510
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-08-11 12:27:20 +0600

    chore(web): bootstrap app

--- web/src/apps/qmmsoft.vue
+++ web/src/apps/qmmsoft.vue
@@ -1,5 +1,5 @@
 <template>
-  <layout-default>
+  <layout-default v-if="isReady">
     <router-view v-slot="{ Component }">
       <transition name="out-in">
         <suspense>
@@ -11,10 +11,38 @@
 </template>
 
 <script lang="ts">
+import { Loading } from 'element-plus/es/components/loading/src/service';
 import LayoutDefault from '#layouts/layout-default.vue';
 
 export default defineComponent({
   name: 'the-qmmsoft-app',
   components: { LayoutDefault },
+  setup() {
+    const authStore = useAuthStore();
+    const settingsStore = useSettingsStore();
+
+    const isReady = ref(false);
+
+    onMounted(async () => {
+      const loading = Loading({
+        fullscreen: true,
+        lock: true,
+        text: 'Getting user info...',
+      });
+
+      await authStore.init();
+
+      loading.text.value = 'Loading settings...';
+
+      await settingsStore.init();
+
+      isReady.value = true;
+      loading.close();
+    });
+
+    return {
+      isReady,
+    };
+  },
 });
 </script>
