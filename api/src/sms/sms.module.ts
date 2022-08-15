71e85bed4f49f90819d2fc80b27f22468b0697e0
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-08-15 10:35:11 +0600

    chore(api)[sms]: remove SmsService from imports

--- api/src/sms/sms.module.ts
+++ api/src/sms/sms.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { SmsService } from './sms.service.js';
 
 @Module({
-  imports: [SmsService],
   providers: [SmsService],
   exports: [SmsService],
 })
